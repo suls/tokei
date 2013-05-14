83eda580197aff5600578d25248a48c5d5150d8a
Mathias Sulser <suls@suls.org>
2013-05-14 22:04:27 +0200

    initial commit

---
+++ src/main/java/org/suls/tokei/App.java
@@ -0,0 +1,13 @@
+package org.suls.tokei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
